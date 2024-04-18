66bcd2be6b80f612d9c4654bb86e74a82cb220eb
edz0 <edson.macaiba@hotmail.com>
2024-04-18 19:00:18 -0300

    feat: complete version

--- README.md
+++ README.md
@@ -27,10 +27,20 @@ O jogo tem a seguinte espeicifcação:
 
 Há dois jogadores: X e O. Um jogador ganha quando completa uma sequência de 3 casas consecutivas iguais, que pode ser horizontal, vertical ou diagnoal.
 
-### ToDo
-- [x] Mostrar tabuleiro.
-- [ ] Realizar jogada.
-- [ ] Game Loop.
-- [ ] Condições de vitória.
+### Compilação
+Para compilar o jogo e executar o código use o comando `cargo run` ou `rustc -o jogodavelha src/main.rs` para compilar e `./jogodavelha` para executar o jogo.
 
 ### Implementação
+Iniciar partida
+  - Ao iniciar a partida o jogo mostra as casas referente a cada número entre 1 e 9.
+  - Qualquer jogada fora desse intervalo é considerada jogada invalida.
+
+Vitória
+  - A verificação de linhas, colunas e diagonal é feita "na mão" para todas as possibilidades.
+  - Como estamos aproveitando o código para verificar tanto o "X" quanto o "O", é preciso adicionar uma condição extra que verifica se o caractere procurado é diferente de _espaço_, o espaço foi usado para inicializar o _array_.
+  - Utilizar uma matriz de duas dimenções ajuda na verificação da vitória.
+
+Empate
+  - O jogo termina empatado quando não tem nenhum espaço disponivel no tabuleiro.
+  - Mesmo que não exista mais possibilidade de vitória, o jogo não termina antes do tabuleiro ser totalmente preenchido.
+  - Foi criado um _for_ que verifica se ainda tem o caractere espaço no tabuleiro, se não há mais então o jogo é encerado.
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,96 @@
 use std::io;
 
-fn board(vector: [[char; 3]; 3]){
-    println!(" {} | {} | {}", vector[0][0], vector[0][1], vector[0][2]);
-    println!("----------");
-    println!(" {} | {} | {}", vector[1][0], vector[1][1], vector[1][2]);
-    println!("----------");
-    println!(" {} | {} | {}", vector[2][0], vector[2][1], vector[2][2]);
+fn board_array(array: [char; 9]){
+    println!(" {} | {} | {}", array[0], array[1], array[2]);
+    println!("-----------");
+    println!(" {} | {} | {}", array[3], array[4], array[5]);
+    println!("-----------");
+    println!(" {} | {} | {}", array[6], array[7], array[8]);
+    /*let mut count: i8 = 1;
+    for index in array{
+        if count == 3 || count == 6{
+            println!(" {index}\n-----------");
+        }else if count == 9{
+            print!(" {}", index);
+        }else{
+            print!(" {} |", index);
+        }
+        count += 1;
+    }
+    println!();*/
 }
+
 fn welcome(){
     println!("Bem-vindo ao Jogo da Velha!");
     println!(" 1 | 2 | 3");
-    println!("----------");
+    println!("-----------");
     println!(" 4 | 5 | 6");
-    println!("----------");
+    println!("-----------");
     println!(" 7 | 8 | 9");
 }
+
+fn win_condition(array: [char; 9]) -> bool{
+    if array[0] == array[1] && array[1] == array[2] && array[0] != ' '
+    || array[3] == array[4] && array[4] == array[5] && array[3] != ' '
+    || array[6] == array[7] && array[7] == array[8] && array[6] != ' '{
+        return true;
+    }else if array[0] == array[3] && array[3] == array[6] && array[0] != ' '
+    || array[1] == array[4] && array[4] == array[7] && array[1] != ' '
+    || array[2] == array[5] && array[5] == array[8] && array[2] != ' '{
+        
+        return true;
+    }else if array[0] == array[4] && array[4] == array[8] && array[0] != ' '
+    || array[2] == array[4] && array[4] == array[6] && array[2] != ' '{
+       
+        return true;
+    }
+    return false; 
+}
+
 fn main() {
-    let mut vector: [[char; 3]; 3] = [[' '; 3]; 3];
+    let mut array: [char; 9] = [' '; 9];
     let mut input = String::new();
-    let c = 'X';
-    
+    let mut c = 'X';
+
     welcome();
  
-    println!("Digite uma posição [1-9] para jogar:");
-    io::stdin().read_line(&mut input).expect("error to read the input!");
-    let play:u32 = input.trim().parse().expect("number invalid!");
-    
-    match play {
-        1 => vector[0][0] = c,
-        2 => vector[0][1] = c,
-        3 => vector[0][2] = c,
-        4 => vector[1][0] = c,
-        5 => vector[1][1] = c,
-        6 => vector[1][2] = c,
-        7 => vector[2][0] = c,
-        8 => vector[2][1] = c,
-        9 => vector[2][2] = c,
-        _ => println!("Posição inválida!")
-    }
+    loop {
+        
+        println!("Digite uma posição [1-9] para jogar:");
+        io::stdin().read_line(&mut input).expect("Erro ao ler a entrada digitada!");
+        let play:usize = input.trim().parse().expect("Número Invalido!");
 
-    board(vector);
+        if array[play-1] == ' '{
+            array[play-1] = c;
+
+            let mut aux: i8 = 0;
+            for index in array{
+                if index == ' '{
+                    aux+=1;
+                }
+            }
+            if aux == 0{
+                board_array(array);
+                println!("Empatou!!!");
+                return;
+            }
+            if win_condition(array){
+                board_array(array);
+                println!("Jogar '{}' Ganhou!!!", c);
+                return;
+            }
+            
+            if c == 'X'{
+                c = 'O';
+            }else if c == 'O'{
+                c = 'X';
+            }
+        }else {
+            println!("Lugar ocupado, por favor escolha outro!");
+        }
+        board_array(array);
+        println!();
+
+        input = String::new();
+    }    
 }
